2bcb53d976d346ab741c021c30614aa70a3e7b17
Ezequiel <ezequiel3434@gmail.com>
2020-06-28 16:10:11 -0300

    Creacion de los Outlets

--- iOS-Calculator/Controller/Home/HomeViewController.swift
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -8,7 +8,34 @@
 
 import UIKit
 
+
+
 final class HomeViewController: UIViewController {
+    
+    // MARK: -Outlets
+    // result
+    @IBOutlet weak var resultLabel: UILabel!
+    //numbers
+    @IBOutlet weak var number0: UIButton!
+    @IBOutlet weak var number1: UIButton!
+    @IBOutlet weak var number2: UIButton!
+    @IBOutlet weak var number3: UIButton!
+    @IBOutlet weak var number4: UIButton!
+    @IBOutlet weak var number5: UIButton!
+    @IBOutlet weak var number6: UIButton!
+    @IBOutlet weak var number7: UIButton!
+    @IBOutlet weak var number8: UIButton!
+    @IBOutlet weak var number9: UIButton!
+    
+    // Operators
+    @IBOutlet weak var opeartorAC: UIButton!
+    @IBOutlet weak var operatorPlusMinus: UIButton!
+    @IBOutlet weak var operatorPercent: UIButton!
+    @IBOutlet weak var operatorResult: UIButton!
+    @IBOutlet weak var operatorAddition: UIButton!
+    @IBOutlet weak var operatorSubstraction: UIButton!
+    @IBOutlet weak var operatorMultiplication: UIButton!
+    @IBOutlet weak var operatorDivision: UIButton!
     
     // MARK: Initializacion
     
@@ -25,7 +52,7 @@ final class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        
     }
 
 
