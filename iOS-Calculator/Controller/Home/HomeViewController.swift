22aedbdd8f4955003ea0f0d0d48cb6ec74571d91
Ezequiel <ezequiel3434@gmail.com>
2020-06-28 12:19:05 -0300

    Creacion del visor de resultados y botones

---
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeViewController.swift
+//  iOS-Calculator
+//
+//  Created by Ezequiel Parada Beltran on 28/06/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    // MARK: Initializacion
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+
+}
